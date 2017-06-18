83e05ff0b899b6405d250ec0f13d14d781367157
Solarex <rh.hou.work@gmail.com>
2017-06-18 16:46:35 +0800

    reflection

--- src/com/solarexsoft/test/proxy/Test.java
+++ src/com/solarexsoft/test/proxy/Test.java
@@ -10,9 +10,19 @@ public class Test {
     public static void main(String[] args) {
         //SolarexInvocationHandler invocationHandler = new SolarexInvocationHandler(new OperateImpl());
         RightInvocationHandler invocationHandler = new RightInvocationHandler(new OperateImpl());
-        Operate operate = (Operate) Proxy.newProxyInstance(Operate.class.getClassLoader(), new Class[]{Operate.class}, invocationHandler);
+        Operate operate = (Operate) Proxy.newProxyInstance(Operate.class.getClassLoader(), OperateImpl.class.getInterfaces(), invocationHandler);
         operate.operateMethod1();
         operate.operateMethod2();
         operate.operateMethod3();
     }
 }
+
+//com.sun.proxy.$Proxy0 extends Proxy implement Operate{
+//  InvocationHandler invocationHandler;
+//  $Proxy0(InvocationHandler h){
+//      invocationHandler = h;//super(h)
+//  }
+//  void operateMethod1(){
+//      invocationHandler.invoke(this, method1, args);
+//  }
+//}
