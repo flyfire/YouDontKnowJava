7c85b3d5ce478aadb46ae678c9fe5864306c9f5a
Solarex <rh.hou.work@gmail.com>
2019-09-03 09:26:47 +0800

    test

--- src/com/solarexsoft/test/generics/Main.java
+++ src/com/solarexsoft/test/generics/Main.java
@@ -1,5 +1,8 @@
 package com.solarexsoft.test.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by houruhou on 2018/6/21.
  */
@@ -9,5 +12,7 @@ public class Main {
         Node n = tn;
         n.setData("Hello");
         Integer x = (Integer) n.data;
+        List<Node> nodes = new ArrayList<Node>();
+        nodes.add(null);
     }
 }
