f44f927fabb303efd66903b93fb508491c6e4f98
Solarex <rh.hou.work@gmail.com>
2020-03-15 21:07:38 +0800

    string

--- src/main/java/com/solarexsoft/test/stringtest/Main.java
+++ src/main/java/com/solarexsoft/test/stringtest/Main.java
@@ -7,11 +7,12 @@ public class Main {
     public static void main(String[] args) {
         try {
             String s = "solarex";
+            System.out.println(s + " ---- " + System.identityHashCode(s));
             Field value = String.class.getDeclaredField("value");
             value.setAccessible(true);
             char[] sValue = (char[]) value.get(s);
             Array.setChar(sValue, 0, 'S');
-            System.out.println(s);
+            System.out.println(s + " ---- " + System.identityHashCode(s));
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
