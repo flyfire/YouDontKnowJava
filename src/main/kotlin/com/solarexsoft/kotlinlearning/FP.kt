ea418f2a7d13d16115b2a6da4272e869131572d4
Solarex <rh.hou.work@gmail.com>
2020-04-27 18:30:28 +0800

    fp

---
+++ src/main/kotlin/com/solarexsoft/kotlinlearning/FP.kt
@@ -0,0 +1,27 @@
+package com.solarexsoft.kotlinlearning
+
+/**
+ * Created by Solarex on 2020/4/27/6:21 PM
+ * Desc:
+ */
+fun play(onLoading: (curVideoId:Int)->Unit, onFinish: () -> Unit) {
+    onLoading(1)
+    onFinish.invoke()
+}
+fun setProgress(onLoading: (curVideoId: Int) -> Unit, onFinish: () -> Unit) {
+    onLoading.invoke(2)
+    onFinish.invoke()
+}
+
+fun onLoading(curVideoId: Int): Unit {
+    println(curVideoId)
+}
+
+fun onFinish():Unit {
+    
+}
+
+fun main() {
+    play(::onLoading, ::onFinish)
+    setProgress(::onLoading) { println("hi") }
+}
